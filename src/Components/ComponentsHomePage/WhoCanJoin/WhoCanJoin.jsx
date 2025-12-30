f89095b651555271a02562afa4de9cb3895417ba
mahmoudfathy <mahmoudfathy.dev@gmail.com>
2025-12-30 12:35:58 +0200

    netfily push2

--- src/Components/ComponentsHomePage/WhoCanJoin/WhoCanJoin.jsx
+++ src/Components/ComponentsHomePage/WhoCanJoin/WhoCanJoin.jsx
@@ -4,13 +4,13 @@ import colleges from '../../../images/colleges.png'
 import person from '../../../images/person.png'
 import Groupicons from '../../../images/startup.png'
 import Corporates from '../../../images/Corporates.png'
-import join from '../../../images/join.png'
+import join from '../../../images/Join.png'
 export default function WhoCanJoin() {
     const joining = [
-        {id:1 , number:"01" , name:"Colleges/Universities", image:colleges},
-        {id:2 , number:"02" , name:"Individuals/Working Professionals", image:person},
-        {id:3 , number:"03" , name:"Startups", image:Groupicons},
-        {id:4 , number:"04" , name:"Corporates", image:Corporates},
+        { id: 1, number: "01", name: "Colleges/Universities", image: colleges },
+        { id: 2, number: "02", name: "Individuals/Working Professionals", image: person },
+        { id: 3, number: "03", name: "Startups", image: Groupicons },
+        { id: 4, number: "04", name: "Corporates", image: Corporates },
     ]
     return (
         <div className='Join-section'>
@@ -25,10 +25,10 @@ export default function WhoCanJoin() {
                         </div>
                         <div className="icons">
                             {
-                                joining.map((item)=>(
+                                joining.map((item) => (
                                     <div key={item.id} className="icon">
                                         <span>{item.number}</span>
-                                        <img src={item.image}/>
+                                        <img src={item.image} />
                                         <p>{item.name}</p>
                                     </div>
                                 ))
